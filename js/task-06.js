b6605ed4df96cb4bd3a326338088026af639cbb4
ihumentsev <a.ihumentsev@gmail.com>
2022-08-09 21:42:45 +0200

    исправлена валидация инпута

--- js/task-06.js
+++ js/task-06.js
@@ -2,8 +2,11 @@ const validInput = document.querySelector("#validation-input");
 
 validInput.addEventListener("blur", (event) => {
   console.log(event.currentTarget.value.length);
-  if (
-    validInput.getAttribute("data-length") > event.currentTarget.value.length
+  if (event.currentTarget.value.length === 0) {
+    validInput.classList.add("#validation-input");
+  } else if (
+    validInput.getAttribute("data-length") > event.currentTarget.value.length ||
+    validInput.getAttribute("data-length") < event.currentTarget.value.length
   ) {
     validInput.classList.add("invalid");
   } else {
